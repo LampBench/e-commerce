91a266258f3cd511d9aabe347baa55f0aa6846a0
Dai Nguyen Trong <dai.nguyentrong@nashtechglobal.com>
2023-02-01 14:17:23 +0700

    Setup SPA render

---
+++ resources/views/spa-view.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHẦN MỀM QUẢN LÝ HỘI NGHỊ NHÀ CHUNG CƯ</title>
+</head>
+<body style="margin: 0;">
+    <div id="root"></div>
+    <script src="{{ mix('/js/index.js')}} "></script>
+    <script src="{{ mix('/js/manifest.js') }}"></script>
+    <script src="{{ mix('/js/vendor.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SpaRenderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('{path?}', [SpaRenderController::class, '__invoke'])->where('path', '[a-zA-Z0-9-/]+');
