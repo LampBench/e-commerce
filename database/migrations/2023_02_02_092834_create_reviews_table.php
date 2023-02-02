18a3bc40059fe996358f5980347a37554a39e1b5
Tang Hien Phan <phan05042000@gmail.com>
2023-02-02 17:01:34 +0700

    Add migrations for project

---
+++ database/migrations/2023_02_02_092834_create_reviews_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('book_id')->constrained('books');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('review_reply_for')->nullable()->constrained('reviews');
+            $table->string('review_title', 120);
+            $table->string('review_details')->nullable();
+            $table->enum('status', [1, 2])->default(1);
+            $table->timestamp('review_date');
+            $table->integer('rating_star')->default(5);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
