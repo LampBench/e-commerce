11c2504e3da6934149af98609f820e76d268926a
Nguyen Tuan Anh <nguyentuananh8736@gmail.com>
2023-02-02 16:37:46 +0700

    console create repository

--- app/Console/Commands/CreateRepository.php
+++ app/Console/Commands/CreateRepository.php
@@ -11,7 +11,7 @@ class CreateRepository extends Command
      *
      * @var string
      */
-    protected $signature = 'make:repository';
+    protected $signature = 'make:repository {name} {--M|model=} {--E|extends=} {--I|implements=}';
 
     /**
      * The console command description.
@@ -27,6 +27,59 @@ class CreateRepository extends Command
      */
     public function handle()
     {
-        return Command::SUCCESS;
+        $name = $this->argument('name');
+        if (!file_exists(app_path('Repositories'))) {
+            mkdir(app_path('Repositories'));
+        }
+
+        $path = app_path('Repositories/' . $name . '.php');
+        if (file_exists($path)) {
+            $this->error('Repository already exists!');
+            return false;
+        }
+
+        $stub = file_get_contents(app_path('Console/Commands/Stubs/CoreRepository.stub'));
+        $stub = str_replace('{namespace}', 'App\Repositories', $stub);
+        $stub = str_replace("{nameRepository}", $name, $stub);
+
+        if ($this->option('model')) {
+            $model =  ucfirst($this->option('model'));
+            $useModel = "\ruse App\Models\\$model;";
+            $stub = str_replace('{useModel}', $useModel, $stub);
+            $stub = str_replace('{model}', $model . ' $model', $stub);
+        } else {
+            $stub = str_replace('{useModel}', '', $stub);
+            $stub = str_replace('{model}', '$model', $stub);
+        }
+
+        if ($this->option('extends')) {
+            $className = $this->option('extends');
+            if (!file_exists(app_path('Repositories/' . $className . '.php'))) {
+                $this->error('This Repositories not exists!');
+                return false;
+            }
+            $extend = "extends $className";
+            $stub = str_replace('{extends}', $extend, $stub);
+            $stub = str_replace('{useExtend}', "\ruse App\Repositories\\" . $className . ';', $stub);
+        } else {
+            $stub = str_replace('{extends}', '', $stub);
+            $stub = str_replace('{useExtend}', '', $stub);
+        }
+
+        if ($this->option('implements')) {
+            $className = $this->option('implements');
+            if (!file_exists(app_path('Interfaces/' . $className . '.php'))) {
+                $this->error('This Interface not exists!');
+                return false;
+            }
+            $implements = "implements $className";
+            $stub = str_replace('{implements}', $implements, $stub);
+            $stub = str_replace('{useInterface}', "\ruse App\Interfaces\\" . $className . ';', $stub);
+        } else {
+            $stub = str_replace('{useInterface}', '', $stub);
+            $stub = str_replace('{implements}', '', $stub);
+        }
+
+        file_put_contents($path, $stub);
     }
 }
