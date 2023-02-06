8e3b3646d048a2fe619fbd936c3e0fd7a8beef3b
Dai Nguyen Trong <dai.nguyentrong@nashtechglobal.com>
2023-02-06 11:51:52 +0700

    Make trait format response

---
+++ app/Traits/RespondsWithHttpStatus.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Traits;
+
+trait RespondsWithHttpStatus
+{
+    protected function respondWithSuccess($data, $message = null, $code = 200)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+
+    protected function respondWithSuccessNoData($message = null, $code = 200)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+        ], $code);
+    }
+
+    protected function respondWithError($message, $code = 400)
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+        ], $code);
+    }
+
+    protected function respondUnauthorized($message = 'Unauthorized')
+    {
+        return $this->respondWithError($message, 401);
+    }
+}
