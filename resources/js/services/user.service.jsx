b65d221d04e90fcd70e3c2f9b12572afe413d61f
Tang Hien Phan <phan05042000@gmail.com>
2023-02-08 17:30:16 +0700

    Implement UI to get view list of user

--- resources/js/services/user.service.jsx
+++ resources/js/services/user.service.jsx
@@ -1,6 +1,10 @@
 import Repository from "./repository";
 
 class UserService extends Repository {
+    async getUsers() {
+        return await this.get("users");
+    }
+
     async create(data) {
         return await this.post("users", data);
     }
