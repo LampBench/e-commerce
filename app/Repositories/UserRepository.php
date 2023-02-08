1914c71affc758286ea3a398117879e5c6f759d6
Tang Hien Phan <phan05042000@gmail.com>
2023-02-08 16:19:54 +0700

    Delete unuse code

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -7,8 +7,6 @@ use App\Models\User;
 
 class UserRepository extends ExpandedBaseRepository
 {
-    protected $model;
-
     public function __construct(User $model)
     {
         $this->model = $model;
--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -7,8 +7,6 @@ use App\Repositories\CategoryRepository;
 
 class CategoryService extends ExpandedBaseService
 {
-    protected $repository;
-
     public function __construct(CategoryRepository $repository)
     {
         $this->repository = $repository;
