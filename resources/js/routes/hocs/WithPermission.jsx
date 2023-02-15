21674a1de936321105878ab28c35dc3d485c8b2d
Dai Nguyen Trong <dai.nguyentrong@nashtechglobal.com>
2023-02-15 09:29:25 +0700

    Fix resource

--- resources/js/routes/hocs/WithPermission.jsx
+++ resources/js/routes/hocs/WithPermission.jsx
@@ -5,7 +5,7 @@ import { Navigate } from "react-router-dom";
 const withPermission = (module, allowedPermission) => (Component) => {
     return (props) => {
         const permissions = useSelector(state => state.user.permissions);
-        if (permissions[module] && permissions[module].includes(allowedPermission)) {
+        if (permissions[module]?.includes(allowedPermission)) {
             return <Component {...props} selfPermission={permissions} />;
         }
         return <Navigate to="/error/access-denied" />;
