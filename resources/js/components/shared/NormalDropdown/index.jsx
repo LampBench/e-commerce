52bee5d10cef0ca31ef96f9703f683557b2a4ea5
Tang Hien Phan <phan05042000@gmail.com>
2023-02-27 10:41:23 +0700

    Add normal dropdown

---
+++ resources/js/components/shared/NormalDropdown/index.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Dropdown, DropdownButton } from "react-bootstrap";
+import './style.scss';
+
+function NormalDropdown({ item, setAPIParams, checkShow, setCheckShow }) {
+
+    const checkAll = (id, checkShowId) => {
+        if (id === "all" || checkShowId.length === item.values.length) {
+            return [];
+        }
+        return checkShowId;
+    }
+
+    const handleClickItem = (id) => {
+        let checkShowId = checkShow;
+        if (item.multiple) {
+            if (checkShowId.includes(id)) {
+                checkShowId.splice(checkShowId.indexOf(id), 1)
+            }
+            else {
+                checkShowId.push(id)
+            }
+            setAPIParams(checkAll(id, checkShowId), item, setCheckShow);
+        }
+        else {
+            setAPIParams([id], item, setCheckShow);
+        }
+    }
+    return (
+        <DropdownButton id="dropdown-item-button" title={item.show}>
+            {item.multiple &&
+                <Dropdown.Item as="button"
+                    active={checkShow.length === 0}
+                    key={item.field + "_all"}
+                    onClick={() => handleClickItem("all")}>
+                    {"All"}
+                </Dropdown.Item>
+            }
+            {item.values.map((value) => {
+                return (
+                    <Dropdown.Item as="button"
+                        active={checkShow.includes(value.id)}
+                        key={item.field + "_" + value.id}
+                        onClick={() => handleClickItem(value.id)}>
+                        {value.name}
+                    </Dropdown.Item>
+                );
+            })}
+        </DropdownButton>
+    );
+}
+
+export default NormalDropdown;
