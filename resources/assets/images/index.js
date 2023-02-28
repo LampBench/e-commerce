1f504838e28360b1c4b3d53f45bed10e08ea7c6c
Tang Hien Phan <phan05042000@gmail.com>
2023-02-28 15:07:20 +0700

    Import images

--- resources/assets/images/index.js
+++ resources/assets/images/index.js
@@ -4,7 +4,10 @@ const images = {
     default_product_image: require('./blank-image.svg').default,
     facebook_icon: require('./facebook-svgrepo-com.svg').default,
     twitter_icon: require('./big-twitter-logo.svg').default,
-    instagram_icon: require('./logo-instagram-svgrepo-com.svg').default
+    instagram_icon: require('./logo-instagram-svgrepo-com.svg').default,
+    ad_sale_1: require('./ad1.jpg').default,
+    ad_sale_2: require('./ad2.png').default,
+    ad_sale_3: require('./ad3.png').default
 };
 
 export default images;
