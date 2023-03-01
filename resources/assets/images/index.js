8732793298ddf15e2ee94052d0932753a1b7e12d
Tang Hien Phan <phan05042000@gmail.com>
2023-03-01 16:52:50 +0700

    Import images

--- resources/assets/images/index.js
+++ resources/assets/images/index.js
@@ -7,7 +7,15 @@ const images = {
     instagram_icon: require('./logo-instagram-svgrepo-com.svg').default,
     ad_sale_1: require('./ad1.jpg').default,
     ad_sale_2: require('./ad2.png').default,
-    ad_sale_3: require('./ad3.png').default
+    ad_sale_3: require('./ad3.png').default,
+    ad_sale_4: require('./ad4.jpg').default,
+    ad_sale_5: require('./ad5.jpg').default,
+    ad_sale_6: require('./ad6.jpg').default,
+    ad_sale_7: require('./ad7.jpg').default,
+    ad_sale_8: require('./ad8.png').default,
+    ad_sale_9: require('./ad9.jpg').default,
+    ad_sale_10: require('./ad10.jpg').default,
+    ad_sale_11: require('./ad11.jpg').default,
 };
 
 export default images;
